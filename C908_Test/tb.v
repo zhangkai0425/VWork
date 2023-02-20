b1476a69066f1393768262a9ee561c87901f21fe
zhangkai0425 <1349605984@qq.com>
2023-02-20 14:33:26 +0800

    删除了不必要的注释

--- C908_Test/tb.v
+++ C908_Test/tb.v
@@ -7,7 +7,6 @@
 
 `define SOC_TOP             tb.x_soc
 `define RTL_MEM             tb.x_soc.x_axi_slave128.x_f_spsram_large
-//`define CPU_TOP             tb.x_soc.x_cpu_sub_system_axi.x_c908_inst
 `define CPU_TOP             tb.x_soc.x_cpu_sub_system
 `define tb_retire0          `CPU_TOP.core0_pad_retire0
 `define retire0_pc          `CPU_TOP.core0_pad_retire0_pc[39:0]
@@ -16,8 +15,6 @@
 `define CPU_CLK             `CPU_TOP.pll_cpu_clk
 `define CPU_RST             `CPU_TOP.pad_cpu_rst_b
 `define clk_en              `CPU_TOP.axim_clk_en
-// `define CP0_RSLT_VLD        `CPU_TOP.x_ct_top_0.x_ct_core.x_ct_cp0_top.x_ct_cp0_iui.cp0_iu_ex3_rslt_vld
-// `define CP0_RSLT            `CPU_TOP.x_ct_top_0.x_ct_core.x_ct_cp0_top.x_ct_cp0_iui.cp0_iu_ex3_rslt_data[63:0]
 
 // `define APB_BASE_ADDR       40'h4000000000
 `define APB_BASE_ADDR       40'hb0000000
@@ -102,30 +99,6 @@ module tb();
       `RTL_MEM.ram15.mem[i][7:0] = mem_inst_temp[j][ 7: 0];
       j = j+1;
     end
-//    i=0;
-//    for(j=0;i<32'h4000;i=j/4)
-//    begin
-//      `RTL_MEM.ram0.mem[i+32'h4000][7:0]  = mem_data_temp[j][31:24];
-//      `RTL_MEM.ram1.mem[i+32'h4000][7:0]  = mem_data_temp[j][23:16];
-//      `RTL_MEM.ram2.mem[i+32'h4000][7:0]  = mem_data_temp[j][15: 8];
-//      `RTL_MEM.ram3.mem[i+32'h4000][7:0]  = mem_data_temp[j][ 7: 0];
-//      j = j+1;
-//      `RTL_MEM.ram4.mem[i+32'h4000][7:0]  = mem_data_temp[j][31:24];
-//      `RTL_MEM.ram5.mem[i+32'h4000][7:0]  = mem_data_temp[j][23:16];
-//      `RTL_MEM.ram6.mem[i+32'h4000][7:0]  = mem_data_temp[j][15: 8];
-//      `RTL_MEM.ram7.mem[i+32'h4000][7:0]  = mem_data_temp[j][ 7: 0];
-//      j = j+1;
-//      `RTL_MEM.ram8.mem[i+32'h4000][7:0]   = mem_data_temp[j][31:24];
-//      `RTL_MEM.ram9.mem[i+32'h4000][7:0]   = mem_data_temp[j][23:16];
-//      `RTL_MEM.ram10.mem[i+32'h4000][7:0]  = mem_data_temp[j][15: 8];
-//      `RTL_MEM.ram11.mem[i+32'h4000][7:0]  = mem_data_temp[j][ 7: 0];
-//      j = j+1;
-//      `RTL_MEM.ram12.mem[i+32'h4000][7:0]  = mem_data_temp[j][31:24];
-//      `RTL_MEM.ram13.mem[i+32'h4000][7:0]  = mem_data_temp[j][23:16];
-//      `RTL_MEM.ram14.mem[i+32'h4000][7:0]  = mem_data_temp[j][15: 8];
-//      `RTL_MEM.ram15.mem[i+32'h4000][7:0]  = mem_data_temp[j][ 7: 0];
-//      j = j+1;
-//    end
   end
 
   initial
@@ -138,8 +111,7 @@ module tb();
     $fwrite(FILE,"TEST FAIL");   
   $finish;
   end
-  
-//  reg [31:0] retire_inst_in_period;
+
   reg [31:0] cycle_count;
   
   `define LAST_CYCLE 50000
@@ -150,32 +122,6 @@ module tb();
     else 
       cycle_count[31:0] <= cycle_count[31:0] + 1'b1;
   end
-  
-  
-//  always @(posedge clk or negedge rst_b)
-//  begin
-//    if(!rst_b) //reset to zero
-//      retire_inst_in_period[31:0] <= 32'b0;
-//    else if( (cycle_count[31:0] % `LAST_CYCLE) == 0)//check and reset retire_inst_in_period every 50000 cycles
-//    begin
-//      if(retire_inst_in_period[31:0] == 0)begin
-//        $display("*************************************************************");
-//        $display("* Error: There is no instructions retired in the last %d cycles! *", `LAST_CYCLE);
-//        $display("*              Simulation Fail and Finished!                *");
-//        $display("*************************************************************");
-//        #10;
-//        FILE = $fopen("run_case.report","w");
-//        $fwrite(FILE,"TEST FAIL");   
-  
-//        $finish;
-//      end
-//      retire_inst_in_period[31:0] <= 32'b0;
-//    end
-//    else if(`tb_retire0 || `tb_retire1)
-//      retire_inst_in_period[31:0] <= retire_inst_in_period[31:0] + 1'b1;
-//  end
-  
-  
   
   reg [31:0] cpu_awaddr;
   reg [3:0]  cpu_awlen;
@@ -191,9 +137,6 @@ module tb();
     cpu_awaddr[31:0] <= `SOC_TOP.x_axi_slave128.mem_addr[31:0];
     cpu_wvalid       <= `SOC_TOP.biu_pad_wvalid;
     cpu_wstrb        <= `SOC_TOP.biu_pad_wstrb;
-    // value0           <= `CPU_TOP.core0_pad_wb0_data[63:0];
-    // value1           <= `CPU_TOP.core0_pad_wb1_data[63:0];
-    // value2           <= `CPU_TOP.core0_pad_wb2_data[63:0];
  
     value0              <= `CPU_TOP.x_C908_TOP.x_pc_top_0.x_pc_core.x_pc_iu_top.x_pc_iu_dp.dp_pipe0_ex4_wb_data[63:0];
     value1              <= `CPU_TOP.x_C908_TOP.x_pc_top_0.x_pc_core.x_pc_iu_top.x_pc_iu_dp.dp_pipe1_ex4_wb_data[63:0];
@@ -225,8 +168,6 @@ module tb();
     end
   
     else if((cpu_awlen[3:0] == 4'b0) &&
-  //     (cpu_awaddr[31:0] == 32'h6000fff8) &&
-  //     (cpu_awaddr[31:0] == 32'h0003fff8) &&
        (cpu_awaddr[31:0] == 32'h01ff_fff0) &&
         cpu_wvalid &&
        `clk_en)
@@ -248,11 +189,7 @@ module tb();
         $write("%c", `SOC_TOP.biu_pad_wdata[103:96]);
      end
     end
-  
   end
- 
-//  parameter cpu_cycle = 110;
-  
   soc x_soc(
     .i_pad_clk           ( clk                  ),
     .b_pad_gpio_porta    (                      ),
@@ -265,28 +202,7 @@ module tb();
     .o_pad_uart0_sout    (                      ),
     .i_pad_rst_b         ( rst_b                )
   );
-  
   int_mnt x_int_mnt(
   );
-  
-// Latest Power control
-//`ifdef UPF_INCLUDED
-//  import UPF::*;
-
-//  initial
-//  begin
-//        supply_on ("VDD", 1.00);
-//     	supply_on ("VDDG", 1.00);
-//  end
-
-//  initial 
-//  begin
-//    $deposit(tb.x_soc.pmu_cpu_pwr_on,  1'b1);
-//    $deposit(tb.x_soc.pmu_cpu_iso_in,  1'b0);
-//    $deposit(tb.x_soc.pmu_cpu_iso_out, 1'b0);
-//    $deposit(tb.x_soc.pmu_cpu_save,    1'b0);
-//    $deposit(tb.x_soc.pmu_cpu_restore, 1'b0);
-//  end
-//`endif
   
 endmodule
