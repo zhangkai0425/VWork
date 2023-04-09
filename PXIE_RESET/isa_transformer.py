f0c23a962f4769115ed7c4669de92445656bcb23
kzhang19 <1349605984@qq.com>
2023-04-09 16:34:25 +0800

    完成了Python指令格式转换脚本

---
+++ PXIE_RESET/isa_transformer.py
@@ -0,0 +1,20 @@
+import os
+
+isa_data = []
+with open('isa.txt', 'r') as f:
+    lines = f.readlines()    # 将文件内容逐行读取到列表lines中
+    for line in lines:
+        inst = line.split(' ')
+        new_inst = [i for i in inst if i.strip()]
+        rdata = [   new_inst[3][6:8]+new_inst[3][4:6]+new_inst[3][2:4]+new_inst[3][0:2],
+                    new_inst[2][6:8]+new_inst[2][4:6]+new_inst[2][2:4]+new_inst[2][0:2],
+                    new_inst[1][6:8]+new_inst[1][4:6]+new_inst[1][2:4]+new_inst[1][0:2],
+                    new_inst[0][6:8]+new_inst[0][4:6]+new_inst[0][2:4]+new_inst[0][0:2],
+                ]
+        isa_data.append(rdata[3])
+        isa_data.append(rdata[2])
+        isa_data.append(rdata[1])
+        isa_data.append(rdata[0])
+        print(isa_data)  # 删除每行前后的空格和换行符，并打印出来
+PXIE_data = isa_data[3]+isa_data[2]+isa_data[1]+isa_data[0]
+print("PXIE_DATA = ",PXIE_data)
